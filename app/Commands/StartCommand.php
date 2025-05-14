02f0e79d6ea09f14889b8a119319afb4eb73f3f9
raimonds-purvinskis <raimonds.purvinskis@printful.com>
2025-05-14 18:15:25 +0300

    Start Command with link

--- app/Commands/StartCommand.php
+++ app/Commands/StartCommand.php
@@ -4,13 +4,32 @@ declare(strict_types=1);
 
 namespace App\Commands;
 
+use Telegram\Bot\Laravel\Facades\Telegram;
 use Telegram\Bot\Objects\Update;
 
 class StartCommand implements CommandInterface
 {
     public function process(Update $update)
     {
-        var_dump("Doing start command");
+        $userId = $update->getMessage()->from->id;
+        $me = Telegram::getMe();
+
+
+        $link = "https://t.me/testpodbot_bot?start=123";
+        Telegram::sendMessage([
+            'chat_id' => $userId,
+            'text' => "Hello, this is a test message from my bot",
+            'reply_markup' => json_encode([
+                'inline_keyboard' => [
+                    [
+                        [
+                            'text' => 'Buy',
+                            'url' => $link,
+                        ],
+                    ],
+                ],
+            ]),
+        ]);
     }
 
     public function getStartState()
