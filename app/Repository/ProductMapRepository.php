c20e6d3a091761bd4ee9225892dc7a526d92d90e
kristaps.taube <kristaps.taube@printful.com>
2025-05-16 13:18:34 +0300

    fix

--- app/Repository/ProductMapRepository.php
+++ app/Repository/ProductMapRepository.php
@@ -94,7 +94,7 @@ class ProductMapRepository
 //                    ],
                 ],
                 'mockup_style_ids' => [
-                    10437, // office env
+                    10423¬, // office env
 //                    10438, // office env
                 ],
                 'placement' => 'default',
